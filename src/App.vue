096b34259e12cff7df258222956077ab1f11c023
hrissolo <hrissolo@gmail.com>
2021-03-03 10:50:48 -0600

    showing up

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
+    <todo-list />
   </div>
 </template>
 
 <script>
+import TodoList from "./components/TodoList";
+
+export default {
+  components: {
+    TodoList,
+  },
+};
 </script>
 
 <style>
