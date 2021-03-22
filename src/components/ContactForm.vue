e0a035115f03f9f00aa7584848fc90738e824d2b
hrissolo <hrissolo@gmail.com>
2021-03-22 11:15:48 -0500

    cont form

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3 class="teal--text">New Contact</h3>
+    <v-form></v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        firstName: "",
+        lastName: "",
+        phone: "",
+        type: "",
+        email: "",
+      },
+    };
+  },
+};
+</script>
+
+<style></style>
