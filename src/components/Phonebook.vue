8ee47f7db37b0ecb33aa96cf9ef3a978f039d297
hrissolo <hrissolo@gmail.com>
2021-03-22 11:12:29 -0500

    contacts section

---
+++ src/components/Phonebook.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Phonebook stuff goes here</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contacts: [],
+    };
+  },
+};
+</script>
+
+<style></style>
